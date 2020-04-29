65fdf236b3f6d3a852d2cbf51d84e1329fdcb2a7
José Angel Azucena <angelvanitas555@gmail.com>
2020-04-29 06:45:06 -0600

    subiendo proyecto

---
+++ webpack/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    entry: './src/app.js',
+    output:{
+        path: path.resolve(__dirname,'../dist'),
+        filename:'bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.(sa|sc|c)ss$/,
+                use:[
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:'./src/index.handlebars',
+            minify:{
+                collapseWhitespace:true,
+                removeComments:true,
+                removeRedundantAttributes:true,
+                removeScriptTypeAttributes:true,
+                removeStyleLinkTypeAttributes:true,
+                useShortDoctype:true
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename:'main.css'
+        })
+    ]
+}
